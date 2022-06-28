0fdb7b2f37b414255b0aa7a100228dee135d6eb0
Your Name <you@example.com>
2022-06-28 19:41:08 +0200

    zdjęciadynamiczne

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
-const h1=document.querySelector("h1")
+const data=[
+    {
+        title: "pink lake",
+        src:"1.webp",
+    },
+    {
+        title: "blue lake",
+        src:"2.webp",
+    },
+    {
+        title: "green",
+        src:"3.png",
+    },
+]
 
-console.log(h1)
+console.log(data)
+
+function changeHTMLInfo(slideNr){
+    const title = data[slideNr].title
+    const titleDOM=document.querySelector("h2")
+    titleDOM.innerHTML=title
+    const image = data[slideNr].src
+    console.log (image)
+    const imageDOM = document.querySelector("img")
+    imageDOM.src = "img/"+image
+}
+
+changeHTMLInfo(2)
